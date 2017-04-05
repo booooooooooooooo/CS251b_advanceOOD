b233d609844090ad344c2f59f85b0eb9058aac49
Bo <bo@Bos-MacBook-Pro.lan>
2017-04-04 18:57:24 -0700

    beautify output

--- DesignAnalyzer/analyzer.js
+++ DesignAnalyzer/analyzer.js
@@ -1,13 +1,14 @@
 
+var tools = require('./tools.js');
+var cli = require('pixl-cli');
 var makeReport = function(projectFile) {
-   var tools = require('./tools.js');
    var project = tools.getModel(projectFile);
    var classes = tools.collectElements(project, "_type", "UMLClass");
    //count total, providers, and clients
    var total = Object.keys(classes).length;
    var statistics = {};
    for(var i in classes) {
-     statistics[classes[i]._id] = {"name":classes[i].name, "providers":[], "clients":[], "instability":null, "responsibility":null, "deviance":null};
+     statistics[classes[i]._id] = {"name":classes[i].name, "providers":[], "clients":[], "stability":null, "responsibility":null, "deviance":null};
    }
    for(var i in classes) {
 	   var asoctns = tools.collectElements(classes[i], "_type", "UMLAssociation");
@@ -27,43 +28,48 @@ var makeReport = function(projectFile) {
        statistics[gens[k].source.$ref].providers.push(gens[k].target.$ref);
 	   }
    }
-   //calculate instability, responsibility and deviance
+   //calculate stability, responsibility and deviance
    for(var key in statistics){
-     statistics[key].instability = (Object.keys(statistics[key].providers).length) / total;
+     statistics[key].stability = 1 - (Object.keys(statistics[key].providers).length) / total;
      statistics[key].responsibility = (Object.keys(statistics[key].clients).length) / total;
-     statistics[key].deviance = Math.abs(1 - statistics[key].instability - statistics[key].responsibility);
+     statistics[key].deviance = Math.abs(statistics[key].stability - statistics[key].responsibility);
    }
    //log results
-   console.log("=======================================================");
-   console.log("instability, responsibility, and deviance of every class: ");
+   var rows = [
+    	[ "ClassName", "stability", "responsibility", "deviance" ]
+    ];
    for(var key in statistics){
-     console.log("id: " + key);
-     console.log("  name: " + statistics[key].name);
-     console.log("  providers: " + statistics[key].providers);
-     console.log("  clients: " + statistics[key].clients);
-     console.log("  instability: " + statistics[key].instability);
-     console.log("  responsibility: " + statistics[key].responsibility);
-     console.log("  deviance: " + statistics[key].deviance);
+    //  console.log("id: " + key);
+    //  console.log("  name: " + statistics[key].name);
+    //  console.log("  providers: " + statistics[key].providers);
+    //  console.log("  clients: " + statistics[key].clients);
+    //  console.log("  stability: " + statistics[key].stability);
+    //  console.log("  responsibility: " + statistics[key].responsibility);
+    //  console.log("  deviance: " + statistics[key].deviance);
+    var row = [statistics[key].name, statistics[key].stability, statistics[key].responsibility,statistics[key].deviance];
+    rows.push(row);
    }
-   //log description
-   console.log("================");
-   console.log("Description:");
-   for(var i in classes) {
-	   console.log("id:" + classes[i]._id);
-     console.log("    name:" + classes[i].name);
-	   var asoctns = tools.collectElements(classes[i], "_type", "UMLAssociation");
-	   console.log("    UMLAssociations:");
-	   for(var k in asoctns) {
-		  console.log("      end1: " + asoctns[k].end1.reference.$ref + "  navigable: " + asoctns[k].end1.navigable);
-      console.log("      end2: " + asoctns[k].end2.reference.$ref + "  navigable: " + asoctns[k].end2.navigable);
-	   }
-     var gens = tools.collectElements(classes[i], "_type", "UMLGeneralization");
-     console.log("    UMLGeneralizations:");
-	   for(var k in gens) {
-		  console.log("      target: " + gens[k].target.$ref);
-      console.log("      source: " + gens[k].source.$ref);
-	   }
-   }
+   cli.print( cli.table(rows) + "\n" );
+
+  //  /*log description*/
+  //  console.log("================");
+  //  console.log("Description:");
+  //  for(var i in classes) {
+	//    console.log("id:" + classes[i]._id);
+  //    console.log("    name:" + classes[i].name);
+	//    var asoctns = tools.collectElements(classes[i], "_type", "UMLAssociation");
+	//    console.log("    UMLAssociations:");
+	//    for(var k in asoctns) {
+	// 	  console.log("      end1: " + asoctns[k].end1.reference.$ref + "  navigable: " + asoctns[k].end1.navigable);
+  //     console.log("      end2: " + asoctns[k].end2.reference.$ref + "  navigable: " + asoctns[k].end2.navigable);
+	//    }
+  //    var gens = tools.collectElements(classes[i], "_type", "UMLGeneralization");
+  //    console.log("    UMLGeneralizations:");
+	//    for(var k in gens) {
+	// 	  console.log("      target: " + gens[k].target.$ref);
+  //     console.log("      source: " + gens[k].source.$ref);
+	//    }
+  //  }
 
 
 };
